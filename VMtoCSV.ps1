824d8dfaaab96379aba39af74e5695fd0c36d401
6abesp <48736810+6abesp@users.noreply.github.com>
2020-02-05 11:08:36 -0600

    Update VMtoCSV.ps1
    error muy tonto e una variable -_-´

--- VMtoCSV.ps1
+++ VMtoCSV.ps1
@@ -8,7 +8,6 @@ $path = read-host "coloca una direccion en tu equipo"
 Add-Content -Path  $path  -Value '"ID","NombreVM","ResourceGroup","subscripcion", "TamaÃ±o","sistema operativo","version", "versiÃ³n","iPrivada","ipPÃºblica","estado","tiempo","red", "subred","dns"'
 
 
-foreach($Subx in $Subsx)
 {
  Set-AzContext -SubscriptionId $Subx
 #Get-AzContext
